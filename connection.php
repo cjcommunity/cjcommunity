303fa9a07c5e83e4c04585f3282075317a340284
cjcommunity <109688373+cjcommunity@users.noreply.github.com>
2022-07-20 23:25:19 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php      
-    $host = "sql303.epizy.com";  
-    $user = "epiz_32205610";  
-    $password = 'BHuLUWCJME6v4';  
-    $db_name = "epiz_32205610_jestocjofficial";  
+    $host = "sql200.epizy.com";  
+    $user = "epiz_32209994";  
+    $password = 'ZOIorWgjOPxbAL';  
+    $db_name = "epiz_32209994_jestocjofficial";  
       
     $conn = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
